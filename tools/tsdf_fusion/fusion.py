a3823dcc35d026402f5620ff25e514fafb6d637b
Huiyu Gao <72381712+huiyu-gao@users.noreply.github.com>
2023-06-02 12:21:46 +1000

    Update fusion.py

--- tools/tsdf_fusion/fusion.py
+++ tools/tsdf_fusion/fusion.py
@@ -312,7 +312,7 @@ class TSDFVolume:
         tsdf_vol, color_vol, weight_vol = self.get_volume()
 
         # Marching cubes
-        verts = measure.marching_cubes_lewiner(tsdf_vol, level=0)[0]
+        verts = measure.marching_cubes(tsdf_vol, level=0)[0]
         verts_ind = np.round(verts).astype(int)
         verts = verts * self._voxel_size + self._vol_origin
 
@@ -332,7 +332,7 @@ class TSDFVolume:
         """
         tsdf_vol, color_vol, weight_vol = self.get_volume()
 
-        verts, faces, norms, vals = measure.marching_cubes_lewiner(tsdf_vol, level=0)
+        verts, faces, norms, vals = measure.marching_cubes(tsdf_vol, level=0)
         verts_ind = np.round(verts).astype(int)
         verts = verts * self._voxel_size + self._vol_origin  # voxel grid coordinates to world coordinates
 
